ee2081d136003cfd14c18a0b4830baabc202c278
bnethanel <bnethanel@gmail.com>
2025-03-18 17:27:30 +0200

    add util file, improve func, add funcs in util file

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 var gBallSize = 100
 function onBallClick() {
     var elBall = document.querySelector('.ball')
+    gBallSize += getRandomIntInclusive(20, 60)
+    elBall.style.backgroundColor = getRandomColor()
 
-
-    if (gBallSize < 400) {
-        gBallSize += 50
-    } else {
+    if (gBallSize > 400) {
         gBallSize = 100
     }
 
     elBall.style.height = gBallSize + 'px'
     elBall.style.width = gBallSize + 'px'
+    elBall.innerHTML = gBallSize
 }
 
 
