9203924e8db81689a7100664686075009f67ced3
bnethanel <bnethanel@gmail.com>
2025-03-18 19:10:53 +0200

    add func for 4th ball

--- js/main.js
+++ js/main.js
@@ -21,8 +21,30 @@ function onBall3Click() {
 
     elball1.style.backgroundColor = ball2Color
     elball2.style.backgroundColor = ball1Color
-    
+
 }
 
+function onBall4Click() {
+    var elBall1 = document.querySelector('.ball1')
+    var elBall2 = document.querySelector('.ball2')
+
+    var decreaseAmount = getRandomIntInclusive(20, 60)
+
+    updateBallSize(elBall1, decreaseAmount)
+    updateBallSize(elBall2, decreaseAmount)
+}
+
+function updateBallSize(elBall, decreaseAmount) {
+    var currSize = parseInt(window.getComputedStyle(elBall).width)
+    var newSize
+
+    var newSize = Math.max(currSize - decreaseAmount, 100)
+
+    elBall.style.width = newSize + 'px'
+    elBall.style.height = newSize + 'px'
+
+    elBall.innerHTML = newSize
+
+}
 
 
