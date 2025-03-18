c61cb63272b55cf2e8baa2b34e742459193a173b
bnethanel <bnethanel@gmail.com>
2025-03-18 16:27:14 +0200

    added script file and improve function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 var gBallSize = 100
 function onBallClick() {
     var elBall = document.querySelector('.ball')
-    gBallSize += 50
+
+
+    if (gBallSize < 400) {
+        gBallSize += 50
+    } else {
+        gBallSize = 100
+    }
+
     elBall.style.height = gBallSize + 'px'
     elBall.style.width = gBallSize + 'px'
 }
